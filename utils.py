0bc666d304e97d32a89d4da720c644b0fd84cc4c
matdomis <matheus.domis2000@gmail.com>
2025-05-20 01:56:50 -0300

    Game Utils

--- utils.py
+++ utils.py
@@ -2,3 +2,4 @@ DEATH = 0                        #☠️
 LIFE = 1                         #❤️
 CELL_SIZE = 10                   #🧱
 CELL_LIFE_PROBABILITY = 0.175    #👾
+FPS = 60                         #🎮
